40a241e57e234976f73b8bdacd781f76a27d8395
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-17 14:33:54 -0700

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+import Component from './component/component.js';
+
+const Index = () => {
+   return (
+      <div>
+        <Component />
+      </div>
+   );
+}
+
+export default Index;
