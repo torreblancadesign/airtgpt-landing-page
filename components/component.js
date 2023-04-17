41ab81b019ba80d7eec74d7bd483c86ff428f5af
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-17 14:33:55 -0700

    Add components/component.js

---
+++ components/component.js
@@ -0,0 +1,23 @@
+
+
+import React, { Component } from 'react';
+
+class AirGPT extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar title="AirGPT" link="www.airtable.com" />
+        <div className="description">
+          <p>
+            AirGPT is a service that lets you deploy websites and apps directly from Airtable to finished product in a matter of seconds.
+          </p>
+        </div>
+        <div className="features">
+          <div className="feature-card">
+            <h3>Fast Deployment</h3>
+            <p>
+              Deploy websites and apps directly from Airtable in a matter of seconds.
+            </p>
+          </div>
+          <div className="feature-card">
+            <h3>
